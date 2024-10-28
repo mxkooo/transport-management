1fd82c70058586bc2f4ec1ad20caf1623a1a9e43
mxkooo <mikoturko@wp.pl>
2024-10-28 16:28:20 +0100

    first commit

---
+++ src/test/java/com/mxkoo/transport_management/TransportManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxkoo.transport_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
